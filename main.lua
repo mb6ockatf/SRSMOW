c97735539b7ccb19630ed490d9d33b52a866497c
mb6ockatf <mb6ockatf@relay.firefox.com>
2023-07-07 23:56:25 +0300

    feat(docs, main.lua): ldoc, request func

--- main.lua
+++ main.lua
@@ -1,4 +1,10 @@
 #!/usr/bin/env lua
+--- some information about solar system
+
+--- print array to stdout.
+-- @function write
+-- @param ... array-like object
+-- @treturn string array
 local function write(...)
 	local text = ""
 	for _, value in ipairs({...}) do
@@ -20,15 +26,27 @@ function dump(o)
 	end
 end
 
+local function split_request_path(request_path)
+    local result = {}
+    for i = 2, #request_path, 1 do
+        local element = request_path[i]
+        if element == "/" then
+            result:insert("")
+        else
+            result[-1] = result[-1] .. element 
+        end
+    return result
+end
+
 local pegasus = require 'pegasus'
 local server = pegasus:new({port="9090", host="127.0.0.1", timeout="3000"})
 
 local function handler(request, response)
 	write("it is running...")
+    file = io.open("shitty_shit", "a")
+    io.output(file)
 	write(request:path())
-	write(dump(request:headers()))
-	write(dump(request.querystring))
+    io.close(file)
 end
 
 server:start(handler)
-
